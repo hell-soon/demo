247e9d84a0b1e129d468758c3c65dfd335a333f7
Maksim1-smg <pmaks2365@gmail.com>
2024-05-23 05:30:56 +0700

    ferst

---
+++ demo/views/site/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
+        <h1 class="display-4">Добро подаловать!</h1>
+
+        <p class="lead">Для продолжения работы на сайта нужно авторизоваться!</p>
+    </div>
+</div>
